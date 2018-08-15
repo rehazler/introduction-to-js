0428de705d06cb9b200233ac54eee01ef6be3e86
rehazler <rehazler@gmail.com>
2018-08-14 20:56:24 -0700

    Added gitignore file and added console log of class expecations and hobby to the js/scripts.js file

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+console.log("I love programming but have had a handful of hiccups in my journey to becoming a skilled developer." + 
+			"In this class I hope to rekindle my love for programming, have a better understanding of JavaScript, " + 
+			"and get to network with a lot of other people in the tech community. \n" +
+			"My favorite hobby, as of late, has been studying music theory, learning to read music, and learning to play " +
+			"the ocarina. I find it to be very relaxing and mentally stimulating.");
